57e23da9801c1f5865f7c1ba8fc0ced43af80310
Rahul Hoque <rhoque@umich.edu>
2024-07-06 23:07:25 -0400

    ok im done for the night

--- colortest.py
+++ colortest.py
@@ -5,19 +5,21 @@ file = "Data.xlsx"
 wb = load_workbook(file)
 
 constants = {
-    '3d85c6': 'Very Good',
-    '7eaeda': 'Good',
-    'bfd7ed': 'Ok',
-    'ffffff': 'Average',
-    'ffaaaa': 'Poor',
-    'ff5555': 'Bad',
-    'ff0000': 'Very Bad'
-
+    '3D85C6': 'Very Good',
+    'FFBED6EB': 'Good',
+    'BFD7ED': 'Ok',
+    'FFFFFFFF': 'Average',
+    'FF000000': 'Average',
+    'FFFF9999': 'Poor',
+    'FF5555': 'Bad',
+    'FF0000': 'Very Bad',
+    '00000000': 'This shouldn"t be here',
+    'FF9900': 'Solar',
+    'FF9900FF': 'Legendary',
+    'FF999999': 'Kinetic'
 }
 
 sheet_names = wb.sheetnames[3:len(wb.sheetnames)-1]
-
-sheet = wb.active
 
 def get_cell_fill_color(cell):
     return cell.fill.start_color.index
@@ -25,14 +27,16 @@ def get_cell_fill_color(cell):
 limiter = 1
 
 for sheet_name in sheet_names:
-    data = pd.read_excel(file, sheet_name=sheet_name, engine='openpyxl')
+    sheet = wb[sheet_name]
+    data = pd.read_excel(file, sheet_name=sheet_name, engine='openpyxl', header=1)
+    data = data.iloc[0:49, 6:27]
     for row in sheet.iter_rows():
         for cell in row:
-            print(cell.fill)
-            limiter += 1
-            if limiter > 10:
-                break
-        if limiter > 10:
-            break
+            print(get_cell_fill_color(cell))
+            print(cell.value)
+            print(constants[get_cell_fill_color(cell)])
+    
+    
+    limiter += 1
     if limiter > 10:
         break
